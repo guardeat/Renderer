a590c18dec6173c4b77a791094e6ab3a08baa500
Gökhan ÇELİK <guardeat@gmail.com>
2025-04-28 20:31:50 +0300

    Minor change.

--- ByteRenderer/opengl_api.h
+++ ByteRenderer/opengl_api.h
@@ -18,26 +18,32 @@ namespace Byte {
     class OpenGLAPI {
     public:
         static void initialize(Window& window) {
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+            static bool initialized{ false };
 
             glfwMakeContextCurrent(window.glfwWindow);
             glfwSetFramebufferSizeCallback(window.glfwWindow, OpenGLAPI::framebufferSizeCallback);
 
-            if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-                throw std::exception{ "GLAD cannot be loaded" };
+            if (!initialized) {
+                glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+                glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+                glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+                if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+                    throw std::exception{ "GLAD cannot be loaded" };
+                }
+
+                glEnable(GL_DEPTH_TEST);
+
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+                stbi_set_flip_vertically_on_load(true);
+
+                initialized = true;
             }
-
-            glEnable(GL_DEPTH_TEST);
-
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-            stbi_set_flip_vertically_on_load(true);
         }
 
         static void update(Window& window) {
